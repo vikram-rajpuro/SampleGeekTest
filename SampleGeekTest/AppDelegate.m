68462ae5c98084824c7f9021b719e91b580ee49c
AMRA <amra@pcv-md5s-mac-mini.local>
2019-03-26 13:05:37 +0530

    fab seq

--- SampleGeekTest/AppDelegate.m
+++ SampleGeekTest/AppDelegate.m
@@ -17,8 +17,31 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    [self makeFabSeqToTenNum];
+    
 }
 
+-(void)makeFabSeqToTenNum{
+    int prev = 0;
+    int sPrev = 0;
+    int sum = 0;
+    for (int a = 1; a<=10; a++) {
+        if(a == 1){
+            prev = 0;
+            sPrev = 0;
+        }
+        else if(a == 2){
+            prev = 0;
+            sPrev = 1;
+        }
+        sum = prev + sPrev;
+        sPrev = prev;
+        prev = sum;
+        
+        NSLog(@"%d", sum);
+    }
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
